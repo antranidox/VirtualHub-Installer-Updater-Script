a4f398c3210b8c11ab158d85d3c4ae1184511146
auckenox <antranidox@me.com>
2016-06-28 01:33:42 +0200

    first commit

---
+++ vh_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "##########################################################"
+echo " VIRTUALHUB INSTALLER-NG / UPDATER-NG SCRIPT, VERSION 3.2 "
+echo "##########################################################"
+
+# you need to be root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root, or Groot :p" 
+   exit 1
+fi
+
+# install dependencies
+echo "Installing dependencies..."
+apt-get install curl php5-cli php5-curl unzip wget -y
+
+wget https://raw.githubusercontent.com/auckenox/VirtualHub-Installer-Updater-Script/master/virtualhub_updater.php -O /tmp/virtualhub_updater.php
+mv /tmp/virtualhub_updater.php /usr/bin/virtualhub_updater
+chmod +x /usr/bin/virtualhub_updater
+/usr/bin/virtualhub_updater
+
+
+
