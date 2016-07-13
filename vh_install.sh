987beb090bbfb27b1922caaf1b407c8ff019cee9
auckenox <antranidox@me.com>
2016-07-13 23:17:09 +0200

    added libusb-1.0.0-dev to dependencies

--- vh_install.sh
+++ vh_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "##########################################################"
-echo " VIRTUALHUB INSTALLER-NG / UPDATER-NG SCRIPT, VERSION 3.2 "
-echo "##########################################################"
+echo "############################################################"
+echo " VIRTUALHUB INSTALLER-NG / UPDATER-NG SCRIPT, VERSION 3.2.1 "
+echo "############################################################"
 
 # you need to be root
 if [[ $EUID -ne 0 ]]; then
@@ -12,7 +12,7 @@ fi
 
 # install dependencies
 echo "Installing dependencies..."
-apt-get install curl php5-cli php5-curl unzip wget -y
+apt-get install curl php5-cli php5-curl unzip wget libusb-1.0.0-dev -y
 
 wget https://raw.githubusercontent.com/auckenox/VirtualHub-Installer-Updater-Script/master/virtualhub_updater.php -O /tmp/virtualhub_updater.php
 mv /tmp/virtualhub_updater.php /usr/bin/virtualhub_updater
