8fc374128ab5d6bd8804f2d48a2d29cbe5eced30
auckenox <antranidox@me.com>
2016-10-12 14:53:22 +0200

    added macOS support

--- vh_install.sh
+++ vh_install.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
 echo "############################################################"
-echo " VIRTUALHUB INSTALLER-NG / UPDATER-NG SCRIPT, VERSION 3.2.1 "
+echo " VIRTUALHUB INSTALLER-NG / UPDATER-NG SCRIPT, VERSION 3.3.0 "
 echo "############################################################"
 
-# you need to be root
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root, or Groot :p" 
-   exit 1
+# os: macOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+curl "https://raw.githubusercontent.com/auckenox/VirtualHub-Installer-Updater-Script/master/virtualhub_updater.php" > /tmp/virtualhub_updater.php
+mv /tmp/virtualhub_updater.php /usr/local/bin/virtualhub_updater
+chmod +x /usr/local/bin/virtualhub_updater
+/usr/local/bin/virtualhub_updater
+exit 0
 fi
 
-# install dependencies
-echo "Installing dependencies..."
-apt-get install curl php5-cli php5-curl unzip wget libusb-1.0.0-dev -y
-
+# os: Linux
 wget https://raw.githubusercontent.com/auckenox/VirtualHub-Installer-Updater-Script/master/virtualhub_updater.php -O /tmp/virtualhub_updater.php
 mv /tmp/virtualhub_updater.php /usr/bin/virtualhub_updater
 chmod +x /usr/bin/virtualhub_updater
-/usr/bin/virtualhub_updater
 
-
+# you need to be root
+if [[ $EUID -ne 0 ]]; then
+   echo "please run this script as root, or install dependencies yourself by typing:"
+   echo "apt-get install curl php5-cli php5-curl unzip wget libusb-1.0.0-dev -y"
+   echo ""
+   echo "and after that start the script by just typing:"
+   echo "virtualhub_updater"
+   exit 1
+else
+# install dependencies
+echo "Installing dependencies..."
+apt-get install curl php5-cli php5-curl unzip wget libusb-1.0.0-dev -y
+exit 0
+fi
 
