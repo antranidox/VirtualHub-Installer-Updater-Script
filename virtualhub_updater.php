b4fe99b7b4b3014a6f44628a48397abbe1adeb68
auckenox <antranidox@me.com>
2016-10-12 14:33:07 +0200

    added macOS support

--- virtualhub_updater.php
+++ virtualhub_updater.php
@@ -1,28 +1,54 @@
 #!/usr/bin/php
 <?php
+define("yVirtualHubURL","http://www.yoctopuce.com/EN/virtualhub.php");
+
+/* --------- yMatchNo ---------
+[0] => /FR/downloads/VirtualHub.windows.25250.zip
+[1] => /FR/downloads/VirtualHub.linux.25250.zip
+[2] => /FR/downloads/VirtualHub.osx.25250.zip
+[3] => /FR/downloads/VirtualHub.qnap.25250.zip
+*/
+
+if(PHP_OS=="Darwin") 
+{
+define("myOS","macOS");
+define("temp_dir","/tmp/");
+$yMatch = "/VirtualHub\\.osx\\.(\\d*).zip/";
+define("yAppLocation","/usr/local/bin/VirtualHub");
+define("yMatchNo",2);
+}
+else
+{
+define("myOS","Linux, ".PHP_OS);
 define("temp_dir","/tmp/");
 if(!file_exists("/usr/bin/unzip")) {echo "Please install unzip first. \n apt-get install unzip -y"; exit;}
+$yMatch = "/VirtualHub\\.linux\\.(\\d*).zip/";
+define("yAppLocation","/usr/sbin/VirtualHub");
+define("yMatchNo",1);
+}
+
+
 $cred="\033[31m"; $cgreen="\033[32m"; $cwhite="\033[0m"; $clblue="\033[34m";
 
 echo $line=str_repeat("-=", 35);
-echo "\n          VirtualHub Linux Installer / Updater Script 1.1\n";
+echo "\n      VirtualHub Linux/macOS Updater / Installer Script 1.2 (".myOS.")\n";
 echo $line."\n\n";
 
 
-$ydlp = file_get_contents("http://www.yoctopuce.com/EN/virtualhub.php");
-preg_match("/VirtualHub\\.linux\\.(\\d*).zip/", $ydlp, $current_version); // getting build-number from website
-$iversion_raw = shell_exec("/usr/sbin/VirtualHub -v");
+$ydlp = file_get_contents(yVirtualHubURL);
+preg_match($yMatch, $ydlp, $current_version); // getting build-number from website
+$iversion_raw = shell_exec(yAppLocation." -v");
 preg_match("/(\\d*) \\(/", $iversion_raw, $installed_version); // extracting only the build-number from local VH
 if(is_numeric($installed_version[1])) { echo "                Installed VirtualHub Build: ".$installed_version[1];} 
-else {echo "                Installed VirtualHub Build: $cred NONE $cwhite";}
+else {echo "                Installed VirtualHub Build: ".$cred." NONE ".$cwhite;}
       echo "\n                Current   VirtualHub Build: ".$current_version[1];
      
 
 // update VH
 if($installed_version[1]!=$current_version[1]) {
-echo "\n\n $clblue $line \n                       a new update is available ...\n $line \n $cwhite";
+echo "\n\n $clblue $line \n                       a new update is available ...\n ".$line." \n ".$cwhite;
 preg_match_all("/<a href='(\\/FR\\/downloads\\/VirtualHub\\.\\S*\\.\\d*.zip)/", $ydlp, $matches); // extracting file link
-$purl = "http://www.yoctopuce.com/".$matches[1][1];
+$purl = "http://www.yoctopuce.com/".$matches[1][yMatchNo];
 echo "\nDownloading VirtualHub Zip file from $purl to ".temp_dir."vh_install/";
 echo $res = shell_exec("curl -k -L $purl -o ".temp_dir."vh.zip");
 if(!file_exists(temp_dir."vh.zip")) {echo "\nfile not found, there seems to be a problem. sorry. ERR-1"; exit;}
@@ -47,8 +73,11 @@ if(!isset($binary_location))
 	exit;
 }
 
-echo "\nInstalling VirtualHub binary...";
-echo $res3 = shell_exec("cp -f -v ".$binary_location." /usr/sbin/VirtualHub");
+echo "\nInstalling VirtualHub binary to ".yAppLocation."...";
+echo $res3 = shell_exec("cp -f -v ".$binary_location." ".yAppLocation);
+
+// not on macOS, only on Linux
+if(PHP_OS!="Darwin") {
 echo "\nInstalling Startscript for VirtualHub...\n";
 echo $res4 = shell_exec("cp -f -v ".temp_dir."vh_install/startup_script/yVirtualHub /etc/init.d/VirtualHub")."\n";
 echo $res5 = shell_exec("chmod +x /etc/init.d/VirtualHub && update-rc.d VirtualHub defaults")."\n";
@@ -58,8 +87,14 @@ echo "\n$clblue Please check http://{THISIP}:4444 in your browser $cwhite \n$lin
 }
 else
 {
-// no update needed
-echo "\n\n$cgreen $line\n           You already have the newest Build of VirtualHub!\n $line \n$cwhite\n\n";
+// on macOS
+echo "VirtualHub is now installed / updated, start it by typing VirtualHub in your terminal.";
 }
 
+}
+else
+{
+// no update needed
+echo "\n\n".$cgreen." ".$line."\n           You already have the newest Build of VirtualHub!\n ".$line." \n".$cwhite."\n\n";
+}
 ?>
